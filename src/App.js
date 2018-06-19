23d604558a7f530bffe9f978bae98e7ad0bac81e
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-18 20:40:37 -0400

    Rendered component and created simple styling with css.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Input from './Input'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
      <div>
-       <h1>Contact Creator</h1>
+       <h1 className = "main-header" >Contact Creator</h1>
+      <Input />
      </div>
     );
   }
