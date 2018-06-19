4f5187fd314ce09dacf0960aaa791817ea7f611b
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-18 20:41:30 -0400

    Used prettier to clean code placement.

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
 
 const Input = () => (
-    <div>
-     <h1>List</h1>
-     <input type="text" placeholder = "Name"/>
-     <input type="number" placeholder = "Number"/>
-     <button>+</button>
-    </div>
-)
+  <div>
+    <h1>List</h1>
+    <input type="text" placeholder="Name" />
+    <input type="number" placeholder="Number" />
+    <button>+</button>
+  </div>
+);
 
-export default Input; 
+export default Input;
