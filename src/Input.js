9c3e2af46b7502faf743769699d4ed8930afb8b7
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-18 22:38:26 -0400

    Created a state for fields that changes upon user input.

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "./App.css";
 
-const Input = () => (
-  <div>
-    <h1>List</h1>
-    <input type="text" placeholder="Name" />
-    <input type="number" placeholder="Number" />
-    <button>+</button>
-  </div>
-);
+class Input extends React.Component {
+    constructor(){
+        super();
+            this.state = {
+                name: "",
+                number: ""
+
+            }   
+            this.onChangeName = this.onChangeName.bind(this)
+            this.onChangeNumber = this.onChangeNumber.bind(this)
+          
+        }
+        onChangeName(event){
+            this.setState({name: event.target.value})
+        }
+        onChangeNumber(event){
+            this.setState({number: event.target.value})
+        }
+    render() {
+        return (
+            <div>
+        <h1>List</h1>
+        <input type="text" placeholder="Name" value = {this.state.name} onChange = {this.onChangeName} />
+        <input type="number" placeholder="Number" value = {this.state.number} onChange = {this.onChangeNumber} />
+        <button>+</button>
+        <h1>Name: {this.state.name} </h1>
+        <h1>Number: {this.state.number}</h1>
+        
+      </div>
+        )
+    }
+};
 
 export default Input;
