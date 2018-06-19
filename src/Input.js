0fe2badc69b891874b67a354b07b5dd1147f3829
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-18 20:39:05 -0400

    Created a stateless functional component for the input field of the app.

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+const Input = () => (
+    <div>
+     <h1>List</h1>
+     <input type="text" placeholder = "Name"/>
+     <input type="number" placeholder = "Number"/>
+     <button>+</button>
+    </div>
+)
+
+export default Input; 
